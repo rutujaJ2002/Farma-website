423db66358cab8825c26656038c81f43b3a641d1
Rutuja Sanjay Jadhav <82172257+rutujaJ2002@users.noreply.github.com>
2022-05-06 19:46:02 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+//menubar
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header-2 .navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+//onclick to producs for pro-description
+document. getElementById("myButton"). onclick = function () {
+    location. href = "https://krjiympbc2wrbppfyzlbgw.on.drv.tw/website/description.html";
+};
